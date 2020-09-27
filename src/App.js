43dc29cc1a42b979a7e20c732d7b1c3d04e1a51b
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2020-09-27 21:24:21 +0100

    started on the question

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Question from './components/question/Question';
+
+function App() {
+  return (
+    <div className="App">
+      <Question 
+        question="This is a question?"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/question/Question.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styles from './Question.module.css';
+
+export default class Question extends Component {
+    state = {
+        rating: 1
+    }
+
+    handleRating = (e) => {
+        this.setState({
+            rating: e.target.value
+        })
+    }
+
+    componentDidUpdate() {
+        console.log(this.state)
+    }
+
+    render() {
+        return(
+            <div className={styles.wrapper}>
+                <h1>{this.props.question}</h1>
+                <input 
+                    type="range" 
+                    id="rating" 
+                    name="rating" 
+                    min="1" 
+                    max="5" 
+                    value={this.state.rating}
+                    onChange={this.handleRating} >
+                </input>
+            </div>
+        )
+    }
+}
