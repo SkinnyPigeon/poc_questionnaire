416b88b6dc630ab9f8417945c22cc0b39c345bd2
skinnypigeon <euan.blackledge@soprasteria.com>
2020-09-28 22:24:15 +0100

    added in the rating and progress trackers

--- src/components/progressTracker/ProgressTracker.js
+++ src/components/progressTracker/ProgressTracker.js
@@ -5,7 +5,8 @@ export default class ProgressTracker extends Component {
     render() {
         return (
             <div className={styles.wrapper}>
-                <input type="range" min="1" max={this.props.max} value={this.props.value} readOnly={true}></input>
+                <input type="range" min="1" max={this.props.max} value={this.props.value} readOnly={true} />
+                <p>Progress: {this.props.value} / {this.props.max}</p>
             </div>
         )
     }
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -9,14 +9,19 @@ export default class Slider extends Component {
     render() {
         return (
             <div className={styles.wrapper}>
-                 <input 
-                    type="range" 
-                    name="rating" 
-                    min="1" 
-                    max="5" 
-                    value={this.props.rating}
-                    onChange={this.handleAnswer} >
-                </input>
+                <div className={styles.slider}>
+                    <p>1</p>
+                    <input
+                        type="range"
+                        name="rating"
+                        min="1"
+                        max="5"
+                        value={this.props.rating}
+                        onChange={this.handleAnswer}
+                    />
+                    <p>5</p>
+                </div>
+        <p>Rating: {this.props.rating} / 5</p>
             </div>
         )
     }
