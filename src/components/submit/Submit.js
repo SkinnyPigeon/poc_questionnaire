1f9ab3ca1b3f4e167ac0671b3ccd230567db85c3
skinnypigeon <euan.blackledge@soprasteria.com>
2020-09-29 15:28:17 +0100

    might have to push up to heroku for testing beyond cors

--- src/components/submit/Submit.js
+++ src/components/submit/Submit.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styles from './Submit.module.css';
 
+const mailjet = require('node-mailjet')
+    .connect(process.env.REACT_APP_APIKEY_PUBLIC, process.env.REACT_APP_APIKEY_PRIVATE)
+
 export default class Submit extends Component {
 
     handleKeyDown = (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             this.handleSubmission()
         }
     }
@@ -13,11 +16,40 @@ export default class Submit extends Component {
         console.log("Submitting")
         let results = this.extractQsAndAs()
         console.log(results)
+        const request = mailjet
+            .post("send", { 'version': 'v3.1' })
+            .request({
+                "Messages": [
+                    {
+                        "From": {
+                            "Email": "2434924@dundee.ac.uk",
+                            "Name": "Euan"
+                        },
+                        "To": [
+                            {
+                                "Email": "euan.blackledge@soprasteria.com",
+                                "Name": "Euan"
+                            }
+                        ],
+                        "Subject": "Serums POC Results Test",
+                        "TextPart": "The following are the results from the POC dev testing",
+                        "HTMLPart": JSON.stringify(results),
+                        "CustomID": "POCTest"
+                    }
+                ]
+            })
+        request
+            .then((result) => {
+                console.log(result.body)
+            })
+            .catch((err) => {
+                console.log(err.statusCode)
+            })
     }
 
     extractQsAndAs = () => {
         let questions = {}
-        for(let key in this.props.questions) {
+        for (let key in this.props.questions) {
             questions[key] = {
                 question: this.props.questions[key].question.props.question,
                 rating: this.props.questions[key].answer
